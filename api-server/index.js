8c1a4f3cd8b9bbe7bf34e4a14604d1d0135bcc9c
Tyler Johnson <tyler@csdconsultants.us>
2021-06-28 13:03:39 -0700

    chose index.js file to use and moved to root and changed paths

--- api-server/index.js
+++ api-server/index.js
@@ -1,8 +1,8 @@
-'use strict';
+// 'use strict';
 
-const { db } = require('./src/models');
-const server = require('../auth-server/src/server.js');
+// const { db } = require('./src/models');
+// const server = require('../auth-server/src/server.js');
 
-db.sync().then(() => {
-  server.start(3000);
-});
+// db.sync().then(() => {
+//   server.start(3000);
+// });
